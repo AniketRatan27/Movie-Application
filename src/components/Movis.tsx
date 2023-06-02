68ad3f00a820a7ceed2fa5b6b8f9d8b81c1e3f07
Aniket K Ratan <122613343+AniketRatan27@users.noreply.github.com>
2023-06-02 17:33:44 +0530

    Update Movis.tsx

--- src/components/Movis.tsx
+++ src/components/Movis.tsx
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 const Movis = () => {
   const { movie } = useMovie();
   return (
-    <SimpleGrid columns={4} spacing={10}>
+    <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} spacing={10}>
       {movie.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
